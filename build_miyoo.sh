4923d1c53c090719d0b50dc02beddde94587b1ec
Ninoh-FOX <ninohfox@gmail.com>
2023-08-09 16:22:31 +0200

    fixed all key files

--- build_miyoo.sh
+++ build_miyoo.sh
@@ -4,6 +4,6 @@ set -ex
 
 make clean
 
-CC="/usr/bin/arm-linux-gnueabihf-gcc" LFLAGS="-lpthread" ./configure --libs /usr/lib/arm-linux-gnueabihf --includes /usr/include/arm-linux-gnueabihf --target-device miyoo --disable=notify-frontend
+CC="/usr/bin/arm-linux-gnueabihf-gcc" LFLAGS="-lpthread" ./configure --libs=/usr/lib/arm-linux-gnueabihf --includes=/usr/include/arm-linux-gnueabihf --target-device=miyoo --disable=notify-frontend
 
 make
